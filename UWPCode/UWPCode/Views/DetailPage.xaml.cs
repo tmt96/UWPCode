bed62c3ff5ff0cb57075bfc747d393be2a5b599a
MinhTuanTran <mt12@williams.edu>
2017-01-08 18:09:32 -0500

    Initial commit

---
+++ UWPCode/UWPCode/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using UWPCode.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPCode.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ UWPCode/UWPCode/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using UWPCode.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace UWPCode.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
